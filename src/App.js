7af20208dca3a0cf0e95ecaa85316322df1336e4
Dario Barros <dario.vita@hotmail.com>
2022-05-18 22:27:54 -0300

    Primer commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Soy un ecommerce onda Steam
         </a>
       </header>
     </div>
