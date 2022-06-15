bd0ea6bebcc8d0978ac8721ab18364615c44c635
xKaige <95537357+xKaige@users.noreply.github.com>
2022-06-14 23:19:14 -0300

    Actualizacion

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,30 +1,27 @@
 import React, { useEffect, useState } from "react";
-import ItemList from "./ItemList"
+import ItemList from "./ItemList";
 
-function ItemListContainer( { onAdd, sumarCarrito }) {
+function ItemListContainer({ onAdd, sumarCarrito }) {
+  const [juegoList, setJuegoList] = useState([]);
 
-    const [juegoList, setJuegoList] = useState([])
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("juegos.json")
+        .then((res) => res.json())
+        .catch((error) => console.error("Error", error))
+        .then((body) => {
+          setJuegoList(body);
+        });
+    }, 2000);
+  }, []);
 
-    useEffect(() => {
-        setTimeout(()=>{
-            fetch("juegos.json")
-                .then(res => res.json())
-                .catch(error => console.error("Error", error))
-                .then( body => { setJuegoList(body)} )
-        }, 2000)
-
-    }, [])
-
-
-    return (
+  return (
     <>
-        <ItemList juegos={juegoList} onAdd={onAdd} sumarCarrito={sumarCarrito} />
+      <ItemList juegos={juegoList} onAdd={onAdd} sumarCarrito={sumarCarrito} />
     </>
-)
+  );
 }
 
-export default ItemListContainer
-
+export default ItemListContainer;
 
 // https://api.rawg.io/api/games?page=2&page_size=10&key=75c173d0dedd439c87e43009cc9c3923
-
